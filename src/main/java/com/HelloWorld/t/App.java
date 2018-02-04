52fa7ebdcf2ee7d8c8f1331b22db5170d987a4e7
mashek347 <masheknyus@gmail.com>
2018-02-04 16:23:56 -0500

    Update maven

--- src/main/java/com/HelloWorld/t/App.java
+++ src/main/java/com/HelloWorld/t/App.java
@@ -4,26 +4,23 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
-import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 /**
  * Hello world!
@@ -56,6 +53,7 @@ public class App {
 		 * driver.get("https://www.google.com/");
 		 */
 		driver = lunch.Url();
+		
 	}
 
 	// Start test cases from here
@@ -95,7 +93,7 @@ public class App {
 		File source = ts.getScreenshotAs(OutputType.FILE);
 		// after execution, you could see a folder "FailedTestsScreenshots"
 		// under src folder
-		String destination = System.getProperty("user.dir") + "/FailedScreenshots/" + screenshotName + dateName
+		String destination = System.getProperty("user.dir") + "/Screenhots/" + screenshotName + dateName
 				+ ".png";
 		File finalDestination = new File(destination);
 		FileUtils.copyFile(source, finalDestination);
--- src/main/java/com/HelloWorld/t/Urls_lunch.java
+++ src/main/java/com/HelloWorld/t/Urls_lunch.java
@@ -1,17 +1,10 @@
 package com.HelloWorld.t;
 
-import java.io.File;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Urls_lunch {
@@ -20,7 +13,7 @@ public class Urls_lunch {
 	public WebDriver Url() {
 		
 		// Chrome Driver only
-	/*	ChromeDriverManager.getInstance().setup();
+/*		ChromeDriverManager.getInstance().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
@@ -38,7 +31,6 @@ public class Urls_lunch {
 		//FireFox
 		
 		WebDriverManager.firefoxdriver().setup();
-		//FirefoxDriverManager.getInstance().setup();
 		FirefoxDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
